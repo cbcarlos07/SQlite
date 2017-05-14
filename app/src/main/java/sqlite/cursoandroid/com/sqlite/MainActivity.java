ad8f76db91cb30feb47322a18b4be0bb002c98a3
Carlos Bruno <cbcarlos08@gmail.com>
2017-05-14 18:32:16 -0400

    Curso de Android

--- app/src/main/java/sqlite/cursoandroid/com/sqlite/MainActivity.java
+++ app/src/main/java/sqlite/cursoandroid/com/sqlite/MainActivity.java
@@ -1,13 +1,53 @@
 package sqlite.cursoandroid.com.sqlite;
 
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+
+
+        SQLiteDatabase bancoDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
+
+        //tabela
+        bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR, idade INT( 3 )) ");
+
+        //Inserir dados
+        bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Marcos',30) ");
+        bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Ana',20) ");
+
+        //recuperar dados
+        Cursor cursor = bancoDados.rawQuery("SELECT nome, idade FROM pessoas ", null);
+
+        //recupera o indice das colunas
+        int indiceColunaNome = cursor.getColumnIndex("nome");
+        int indiceColunaIdade = cursor.getColumnIndex("idade");
+
+        cursor.moveToFirst();
+
+        while ( cursor != null ){
+
+            Log.i("RESULTADO - nome: ", cursor.getString( indiceColunaNome ));
+            Log.i("RESULTADO - idade: ", cursor.getString( indiceColunaIdade ));
+
+           cursor.moveToNext();
+        }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
     }
 }
